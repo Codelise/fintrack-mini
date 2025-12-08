e7fc6dbcb890780e9eeabd089f6c99e466bd075d
Codelise <claycaluscos@gmail.com>
2025-12-08 09:47:41 +0800

    Pulled from Activity 7

---
+++ .next/static/chunks/src_app_pages_dashboard_budget_page_607bb244.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_38e76ae2._.js",
+  "static/chunks/node_modules_930f713e._.js"
+],
+    source: "dynamic"
+});
